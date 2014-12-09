0c77ebb9ddc4c8b7c1f90ba84f02cd0f192211f3
kazeto <kazeto@ecei.tohoku.ac.jp>
2014-12-09 18:49:28 +0900

    Updated phillip's version.

--- src/phillip.cpp
+++ src/phillip.cpp
@@ -12,7 +12,7 @@ namespace phil
 
 
 int phillip_main_t::ms_verboseness = 0;
-const std::string phillip_main_t::VERSION = "phil.2.12";
+const std::string phillip_main_t::VERSION = "phil.2.13";
 
 
 phillip_main_t::phillip_main_t()
