33fc924785239b009c825ae4aa21350441318753
kazeto <kazeto@ecei.tohoku.ac.jp>
2015-10-06 22:12:10 +0900

    Implemented sol::gurobi_k_best_t. (halfway)

--- src/sol/ilp_solver.h
+++ src/sol/ilp_solver.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <memory>
 
 #include "../phillip.h"
 
@@ -103,15 +104,33 @@ public:
     virtual std::string repr() const { return "gurobi-optimizer"; }
     virtual bool do_keep_validity_on_timeout() const override { return false; }
 
-private:
+protected:
 #ifdef USE_GUROBI
+    struct model_t
+    {
+        model_t(const ilp::ilp_problem_t *p) : prob(p) {}
+
+        std::chrono::time_point<std::chrono::system_clock> begin;
+        const ilp::ilp_problem_t *prob;
+        std::unique_ptr<GRBModel> model;
+        GRBEnv env;
+        hash_map<ilp::variable_idx_t, GRBVar> vars;
+        hash_set<ilp::constraint_idx_t> lazy_cons;
+        bool do_cpi;
+    };
+
+    void prepare(model_t&) const;
+    ilp::ilp_solution_t optimize(model_t&) const;
+
+    double get_timeout(std::chrono::time_point<std::chrono::system_clock> begin) const;
+
     void add_variables(
         const ilp::ilp_problem_t *prob,
         GRBModel *model, hash_map<ilp::variable_idx_t, GRBVar> *vars) const;
     void add_constraint(
-        const ilp::ilp_problem_t *prob,
-        GRBModel *model, ilp::constraint_idx_t idx,
+        GRBModel *model, ilp::constraint_t cons,
         const hash_map<ilp::variable_idx_t, GRBVar> &vars) const;
+
     ilp::ilp_solution_t convert(
         const ilp::ilp_problem_t *prob,
         GRBModel *model, const hash_map<ilp::variable_idx_t, GRBVar> &vars,
@@ -119,6 +138,35 @@ private:
 #endif
     int m_thread_num;
     bool m_do_output_log;
+};
+
+
+/** A class of ilp_solver which outputs k-best solutions with Gurobi-optimizer. */
+class gurobi_k_best_t : public gurobi_t
+{
+public:
+    struct generator_t : public component_generator_t<ilp_solver_t>
+    {
+        virtual ilp_solver_t* operator()(const phillip_main_t*) const override;
+    };
+
+    gurobi_k_best_t(
+        const phillip_main_t *ptr, int thread_num, bool do_output_log,
+        int max_num, float threshold, int margin);
+
+    virtual void execute(std::vector<ilp::ilp_solution_t> *out) const;
+    virtual void solve(
+        const ilp::ilp_problem_t *prob,
+        std::vector<ilp::ilp_solution_t> *out) const;
+
+    virtual bool is_available(std::list<std::string> *error_messages) const;
+    virtual std::string repr() const { return "gurobi-k-best"; }
+    virtual bool do_keep_validity_on_timeout() const override { return false; }
+
+private:
+    int m_max_num;
+    float m_threshold;
+    int m_margin;
 };
 
 
---
+++ src/sol/sol_gurobi_kbest.cpp
@@ -0,0 +1,138 @@
+#include <mutex>
+#include <algorithm>
+#include "./ilp_solver.h"
+
+
+namespace phil
+{
+
+namespace sol
+{
+
+
+gurobi_k_best_t::gurobi_k_best_t(
+    const phillip_main_t *ptr, int thread_num, bool do_output_log,
+    int max_num, float threshold, int margin)
+    : gurobi_t(ptr, thread_num, do_output_log),
+    m_max_num(max_num), m_threshold(threshold), m_margin(margin)
+{}
+
+
+void gurobi_k_best_t::execute(std::vector<ilp::ilp_solution_t> *out) const
+{
+#ifdef USE_GUROBI
+    const ilp::ilp_problem_t *prob = phillip()->get_ilp_problem();
+    solve(prob, out);
+#else
+    out->push_back(ilp::ilp_solution_t(
+        prob, ilp::SOLUTION_NOT_AVAILABLE,
+        std::vector<double>(prob->variables().size(), 0.0)));
+#endif
+}
+
+
+void gurobi_k_best_t::solve(
+    const ilp::ilp_problem_t *prob,
+    std::vector<ilp::ilp_solution_t> *out) const
+{
+    const pg::proof_graph_t *graph = prob->proof_graph();
+
+#ifdef USE_GUROBI
+    model_t m(prob);
+    prepare(m);
+
+    while (out->size() < m_max_num)
+    {
+        if (not out->empty())
+        {
+            ilp::constraint_t con(
+                util::format("margin:sol(%d)", out->size()), ilp::OPR_GREATER_EQ);
+            const ilp::ilp_solution_t &sol = out->back();
+            int count(0);
+
+            for (pg::node_idx_t i = 0; i < graph->nodes().size(); ++i)
+            {
+                ilp::variable_idx_t v = prob->find_variable_with_node(i);
+                if (v >= 0)
+                {
+                    if (prob->node_is_active(sol, i))
+                    {
+                        con.add_term(v, -1.0);
+                        ++count;
+                    }
+                    else
+                        con.add_term(v, 1.0);
+                }
+            }
+
+            con.set_bound((double)(m_margin - count));
+            add_constraint(m.model.get(), con, m.vars);
+        }
+
+        ilp::ilp_solution_t sol = optimize(m);
+
+        if (out->empty())
+        {
+            if (sol.type() == ilp::SOLUTION_NOT_AVAILABLE)
+                break;
+
+            // IF DELTA IS BIGGER THAN THRESHOLD, THIS SOLUTION IS NOT ACCEPTABLE.
+            if (m_threshold >= 0.0)
+            {
+                double delta =
+                    sol.value_of_objective_function() -
+                    out->front().value_of_objective_function();
+                if (std::abs(delta) > m_threshold)
+                    break;
+            }
+        }
+
+        out->push_back(sol);
+
+        if (sol.type() == ilp::SOLUTION_NOT_AVAILABLE) break;
+
+        // TODO: âΩÇ‡êÑò_ÇµÇ»Ç¢èÛë‘ÇÊÇËÇ‡ï]âøílÇ™è¨Ç≥Ç¢èÍçáÇÕä¸ãp
+    }
+
+#endif
+}
+
+
+bool gurobi_k_best_t::is_available(std::list<std::string> *error_messages) const
+{
+    if (gurobi_t::is_available(error_messages))
+    {
+        if (m_max_num < 1)
+        {
+            error_messages->push_back(
+                "gurobi_k_best_t::m_max_num must be bigger than 0.");
+            return false;
+        }
+
+        if (m_margin < 1)
+        {
+            error_messages->push_back(
+                "gurobi_k_best_t::m_margin must be bigger than 0.");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+ilp_solver_t* gurobi_k_best_t::generator_t::operator()(const phillip_main_t *ph) const
+{
+    return new sol::gurobi_k_best_t(
+        ph,
+        ph->param_int("gurobi_thread_num"),
+        ph->flag("activate_gurobi_log"),
+        ph->param_int("max_sols_num", 1),
+        ph->param_float("sols_threshold"),
+        ph->param_int("sols_margin", 1));
+}
+
+
+}
+
+}
